8e306d65632147a4e844fdfe61eaccbd05ee6f14
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2026-03-10 23:08:11 +0000

    Tue, Mar 10, 2026, 4:08 PM -07:00

--- script/interactivewebpage.js
+++ script/interactivewebpage.js
@@ -1,15 +1,13 @@
+box = document.querySelector('.box')
+body = document.querySelector('body')
 
-    box = document.querySelector('.box');
-    body = document.querySelector('body');
+colors = ['blue', 'green', 'purple', 'red', 'pink', 'orange', 'yellow']
+let index = 0
 
-    colors = ['blue', 'green', 'purple', 'red', 'pink', 'orange', 'yellow'];
-    index = 0;
-
-    box.addEventListener('click', () => {
-        body.style.background = colors[index];
-        index++;
-        if (index == colors.length) {
-            index = 0;
-        }
-    });
-
+box.addEventListener('click', () => {
+    body.style.background = colors[index]
+    index = index + 1
+    if (index == colors.length){
+        index = 0
+    }
+})
