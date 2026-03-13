d66e4b22134d303e72244f3b9a073a153ab6bb2b
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2026-03-13 16:35:26 +0000

    Fri, Mar 13, 2026, 9:35 AM -07:00

---
+++ script/sound.js
@@ -0,0 +1,27 @@
+buttons = document.querySelector('.buttons')
+
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+for (let i = 0; i < 6; i = i + 1) {
+    btn = document.createElement('button')
+    btn.innerText = sounds[i]
+    btn.classList.add('btn')
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.querySelector(`.${sounds[i]}`).play()
+    })
+    buttons.appendChild(btn)
+}
+
+function stopSongs(){
+    for (let i = 0; i < sounds.length; i = i + 1)
+    {
+        document.querySelector(`.${sounds[i]}`).pause()
+        document.querySelector(`.${sounds[i]}`).currentTime = 0
+    }
+}
+
+
+
+
+
