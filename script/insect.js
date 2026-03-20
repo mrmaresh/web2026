39d54246bad106eda72e707c2a5c51f26d9c2b9e
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2026-03-20 17:01:58 +0000

    Fri, Mar 20, 2026, 10:01 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -32,6 +32,10 @@ function createInsect() {
     insect.style.left = `${x}px`
     insect.innerHTML = `<img src="${src}" alt="${alt}" style="transform: rotate(${Math.random()*360}deg)">`
     game_container.appendChild(insect)
+
+    insect.addEventListener('click', () => {
+        console.log("You clicked me!")
+    }
 }
 
 function getRandomLocation() {
