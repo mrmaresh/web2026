8625e2cfd82cbb5447f5b4442d5222c0ee1a2ce2
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2026-03-24 09:59:52 -0700

    insect game

--- script/insect.js
+++ script/insect.js
@@ -2,7 +2,11 @@ screens = document.querySelectorAll('.screen')
 choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
 start_btn = document.getElementById('start-btn')
 game_container = document.querySelector('.game-container')
-
+scoreEl = document.getElementById('score')
+timeEl = document.getElementById('time')
+messageEl = document.getElementById('message')
+score = 0
+seconds = 0
 
 start_btn.addEventListener('click', () => {
     screens[0].classList.add('up')
@@ -21,17 +25,20 @@ for (let i = 0; i < choose_insect_btns.length; i = i + 1)
 
 function startGame() {
     setTimeout(createInsect, 1000)
+    setInterval(increaseTime, 1000)
 }
 
 function createInsect() {
-    console.log("hello")
-    insect = document.createElement('div')
+    const insect = document.createElement('div')
     insect.classList.add('insect')
     const {x,y} = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
     insect.innerHTML = `<img src="${src}" alt="${alt}" style="transform: rotate(${Math.random()*360}deg)">`
     game_container.appendChild(insect)
+    insect.addEventListener('click', () => {
+        catchInsect(insect)
+    })
 }
 
 function getRandomLocation() {
@@ -43,9 +50,36 @@ function getRandomLocation() {
 }
 
 
-function catchInsect() {
-    //increaseScore()
-    this.classList.add('caught')
-    setTimeout( () => this.remove(), 2000)
-    
+function catchInsect(insect) {
+    increaseScore()
+    insect.classList.add('caught')
+    setTimeout( () => insect.remove(), 2000)
+    addInsects()
+}
+
+function addInsects() {
+    setTimeout(createInsect, 1000)
+    setTimeout(createInsect, 1500)
+}
+
+function increaseScore() {
+    score = score + 1
+    scoreEl.innerHTML = `Score: ${score}`
+    if (score == 30){
+        messageEl.classList.add('visible')
+    }
+}
+
+
+function increaseTime() {
+    seconds = seconds + 1
+    m = Math.floor(seconds/60)
+    s = seconds % 60
+    if (m < 10){
+        m = `0${m}`
+    }
+    if (s < 10){
+        s = `0${s}`
+    }
+    timeEl.innerHTML = `Time: ${m}:${s}`
 }
